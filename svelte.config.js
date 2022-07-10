868dfe876fc9bb264453a88527e52efa5ecd20bd
Ishan Parlikar <iparlika@emich.edu>
2022-07-10 13:19:26 +0530

    updated adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,9 +12,6 @@ const config = {
 			fallback: 'index.html',
 			precompress: false
 		}),
-		prerender: {
-			default: false
-		}
 	}
 };
 
