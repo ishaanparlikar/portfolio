596f092ca0c7bb3470555b5e55e74b424bb63f83
Ishan Parlikar <iparlika@emich.edu>
2022-07-11 19:53:11 +0530

    Created rod Branch and Updated template

--- src/components/organisms/Banner.svelte
+++ src/components/organisms/Banner.svelte
@@ -15,8 +15,8 @@
 		class="px-32 flex flex-col items-end justify-center"
 	>
 		<small class="text-accent font-secondary text-lg mb-7">Hi, my name is</small>
-		<h1 class="text-7xl text-neutral mt-5 mb-10 font-primary font-bold">Ishan Parlikar</h1>
-		<h2 class="text-7xl text-base-100">And I am a web developer</h2>
+		<h1 class="text-7xl text-neutral mt-5 mb-10 font-primary font-bold">{`NAME`}</h1>
+		<h2 class="text-7xl text-base-100">And I am a ...</h2>
 
 		<div class="my-12" in:fly={{ x: 1000, duration: 1500 }}>
 			<Button>Check out my projects</Button>
