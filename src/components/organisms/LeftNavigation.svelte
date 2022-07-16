fce965c421a3a3aaa7adac78dfcd087b70fe560e
hazelnut <iparlika@emich.edu>
2022-07-16 19:54:14 +0530

    Local State added to left navbar

--- src/components/organisms/LeftNavigation.svelte
+++ src/components/organisms/LeftNavigation.svelte
@@ -10,7 +10,7 @@ import { onMount } from 'svelte';
 	})
 </script>
 {#if animation}
-<div transition:fly="{{ y: 200, duration: 2000 }}" class="relative w-24 h-full">
+<div transition:fly|local ="{{ y: 200, duration: 2000 }}" class="relative w-24 h-full">
 	<div class="w-full h-full absolute px-2 flex flex-col justify-end  items-center">
 		<span class="py-6 h-full">
 			<hr class="bg-accent w-[1px] h-full" />
@@ -18,19 +18,19 @@ import { onMount } from 'svelte';
 		<ul class="flex flex-col h-1/3 justify-between">
 			<li
 				class="text-base-100 hover:text-neutral bg-primary shadow shadow-inner rounded-md hover:bg-secondary flex items-center p-2"
-				transition:fly="{{ x: 200, duration: 2300 }}"
+				transition:fly|local ="{{ x: 200, duration: 2300 }}"
 			>
 				<Icon height="30" width="30" icon="iconoir:github" />
 			</li>
 			<li
 				class="text-base-100 hover:text-neutral bg-primary shadow shadow-inner rounded-md hover:bg-secondary flex items-center p-2"
-				transition:fly="{{ x: 300, duration: 2500 }}"
+				transition:fly|local ="{{ x: 300, duration: 2500 }}"
 			>
 				<Icon height="30" width="30" icon="dashicons:linkedin" />
 			</li>
 			<li
 				class="text-base-100 hover:text-neutral bg-primary shadow shadow-inner rounded-md hover:bg-secondary flex items-center p-2"
-				transition:fly="{{ x: 400, duration: 2800 }}"
+				transition:fly|local ="{{ x: 400, duration: 2800 }}"
 			>
 				<Icon height="30" width="30" icon="cib:dev-to" />
 			</li>
