31187d13aad7167c63b965adbee7f6b6e511cc18
hazelnut <iparlika@emich.edu>
2022-07-16 19:53:49 +0530

    Progress Component added

---
+++ src/components/atoms/Progress.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+	import { onMount } from 'svelte';
+	export let value: number;
+
+	const progress = tweened(0, {
+		duration: value*50,
+		easing: cubicOut
+	});
+
+	onMount(() => {
+		progress.set(value);
+	});
+</script>
+
+
+<div class="flex items-center gap-10">
+  <progress class="progress progress-neutral w-56" value={$progress} max="100" />
+</div>
