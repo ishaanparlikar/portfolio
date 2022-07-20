a9995ba46a635271e6c73c36e5f058e869bfcab5
hazelnut <iparlika@emich.edu>
2022-07-20 10:32:50 +0530

    removed styles and used tailwind class

--- src/components/atoms/Button.svelte
+++ src/components/atoms/Button.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	export let href: any='';
 </script>
 
-<button
-	class={`btn bg-transparent border border-accent text-accent border-2 hover:text-primary hover:bg-accent hover:border-neutral`}
->
-	<slot />
-</button>
+{#if href}
+	<a
+		class={`btn bg-transparent border border-accent text-accent border-2 hover:text-primary hover:bg-accent hover:border-neutral`}
+		href={`${href}`}
+	>
+		<slot />
+	</a>
+{:else}
+	<button
+		class={`btn bg-transparent border border-accent text-accent border-2 hover:text-primary hover:bg-accent hover:border-neutral leading-[3.3]`}
+	>
+		<slot />
+	</button>
+{/if}
 
-<style>
-	button {
-		line-height: 3.3;
-	}
-</style>
