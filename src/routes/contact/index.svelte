8a6a2bbc2041bc330ef148f0660bc1eabc1fd037
hazelnut <iparlika@emich.edu>
2022-07-29 01:47:09 +0530

    updated contact page

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -1,25 +1,25 @@
-<script>
+<script lang="ts">
 	import Button from '/src/components/atoms/Button.svelte';
-	import { t, locale, locales } from '/locales/i18n';
-
 	import { getAnalytics, logEvent } from 'firebase/analytics';
 	import { onMount } from 'svelte';
 	import { app } from '../../firebase';
-
 	onMount(() => {
 		const analytics = getAnalytics(app);
 		logEvent(analytics, 'page_title');
 	});
+	let form: Boolean = false;
 </script>
 
 <svelte:head>
 	<title>Contact</title>
 </svelte:head>
 
-<div class="flex flex-col items-center justify-center max-w-sm mx-auto">
-	<h1 class="text-5xl text-accent mb-10">Get In touch</h1>
-	<p class="text- text-secondary font-secondary text-center mb-10">
-		if you want to say Hello or If you have any questions, My inbox is always open for queries
-	</p>
-	<Button style="text-base font-secondary h-auto"><a href="/">Say Hi 👋🏼</a></Button>
+<div class="max-w-xl mx-auto">
+	<div class="flex flex-col items-center justify-center ">
+		<h1 class="text-5xl text-accent mb-10">Get In touch</h1>
+		<p class="text- text-secondary font-secondary text-center mb-10">
+			if you want to say Hello or If you have any questions, My inbox is always open for queries
+		</p>
+		<Button href="/contact/form">Say Hi 👋🏼</Button>
+	</div>
 </div>
