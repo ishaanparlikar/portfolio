7ec3d5836d0fb440a2f21d9235092c2358cb5445
hazelnut <iparlika@emich.edu>
2022-07-29 01:22:09 +0530

    Onclick handler on button

--- src/components/atoms/Button.svelte
+++ src/components/atoms/Button.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	export let href: any='';
+	export let onClickEvent:Function=new Function;
 </script>
 
 {#if href}
@@ -12,6 +13,7 @@
 {:else}
 	<button
 		class={`btn bg-transparent border border-accent text-accent border-2 hover:text-primary hover:bg-accent hover:border-neutral leading-[3.3]`}
+		on:click={onClickEvent()}
 	>
 		<slot />
 	</button>
