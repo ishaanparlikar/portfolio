9a7bbb9aaf748b99020f17f9278827c7c10a18b1
hazelnut <iparlika@emich.edu>
2022-07-30 00:21:56 +0530

    Added Work Json for Timeline

--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -1,7 +1,19 @@
 <script>
-		import { getAnalytics, logEvent } from 'firebase/analytics';
+	import { getAnalytics, logEvent } from 'firebase/analytics';
 	import { onMount } from 'svelte';
 	import { app } from '../../firebase';
+	import works from '/static/data/Work.json';
+
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent,
+		TimelineOppositeContent
+	} from 'svelte-vertical-timeline';
+
 
 	onMount(() => {
 		const analytics = getAnalytics(app);
@@ -12,3 +24,23 @@
 <svelte:head>
 	<title>Work History</title>
 </svelte:head>
+
+<Timeline position="alternate">
+	{#each works as work, i}
+		<TimelineItem style="margin:15px 0">
+			<TimelineOppositeContent slot="opposite-content">
+				<p class="text-neutral">{work.year_start+'-'+work.year_end}</p>
+				<p class="font-secondary">{work.job_title}</p>
+			</TimelineOppositeContent>
+			<TimelineSeparator>
+				<TimelineDot
+					style={`background-color: ${i % 2 == 0 ? 'var(--accent)' : 'var(--light)'};`}
+				/>
+				<TimelineConnector />
+			</TimelineSeparator>
+			<TimelineContent>
+				<h3 class="text-accent font-bold font-secondary">{work.company}</h3>
+			</TimelineContent>
+		</TimelineItem>
+	{/each}
+</Timeline>
